0c6ff7986bcf1a32d6c623dfd03574f03fa20180
gx <375870381@qq.com>
2020-05-11 15:56:17 +0800

    测试多个自定义Realm的authenticationStrategy策略

---
+++ src/main/java/com/gx/shiro/realm/MyRealm1.java
@@ -0,0 +1,52 @@
+package com.gx.shiro.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * @ClassName CustomRealmImplementRealm
+ * @Description 自定义realm第一个 - 配合多个Realm测试
+ * @Authtor guoxiang
+ * @Date 2020/5/11 10:28
+ **/
+public class MyRealm1 implements Realm {
+    /**
+     * 返回一个唯一的Realm名字
+     * @return
+     */
+    @Override
+    public String getName() {
+        return "MyRealm1";
+    }
+
+    /**
+     * 判断此Realm是否支持某些Token
+     * @param token
+     * @return
+     */
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        //仅支持UsernamePasswordToken类型的Token
+        return token instanceof UsernamePasswordToken;
+    }
+
+    /**
+     * 根据Token获取认证信息
+     * @param token
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String)token.getPrincipal();  //得到用户名
+        String password = new String((char[])token.getCredentials()); //得到密码
+        if(!"gx".equals(username)) {
+            throw new UnknownAccountException(); //用户名错误
+        }
+        if(!"123456".equals(password)) {
+            throw new IncorrectCredentialsException(); //密码错误
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
